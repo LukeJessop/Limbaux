fbe7931c134a9cfbe53c5614454666a36c46b50d
LukeJessop <jessop.lucas@gmail.com>
2025-07-27 23:29:12 -0600

    Working on body physics

---
+++ Assets/FindCenter.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FindCenter : MonoBehaviour
+{
+    public Transform[] parts = new Transform[4];
+    void Update()
+    {
+        float avgPointX = (parts[0].position.x + parts[1].position.x + parts[2].position.x + parts[3].position.x) / 4;
+        float avgPointY = (parts[0].position.y + parts[1].position.y + parts[2].position.y + parts[3].position.y) / 4;
+        
+        transform.position = Vector3.Lerp(transform.position, new Vector3(avgPointX, avgPointY, transform.position.z), Time.deltaTime);
+    }
+}
