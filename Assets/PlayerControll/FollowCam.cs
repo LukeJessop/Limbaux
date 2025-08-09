8ce2f5ff253b022657564934d29d035a06783c1a
LukeJessop <jessop.lucas@gmail.com>
2025-08-09 01:43:54 -0600

    pushing what i have

--- Assets/PlayerControll/FollowCam.cs
+++ Assets/PlayerControll/FollowCam.cs
@@ -8,7 +8,7 @@ public class FollowCam : MonoBehaviour
 {
     // public GameObject controller;
     // private FollowMouse controllerScript;
-    public GameObject target;
+    public FollowMouse FollowMouse;
 
     private void Start()
     {
@@ -19,7 +19,8 @@ public class FollowCam : MonoBehaviour
     void Update()
     {
         // target = controllerScript.apendages[controllerScript.currentApendage];
-        var targetPos = new Vector3(target.transform.position.x, target.transform.position.y, transform.position.z);
+        var targetPos = new Vector3(FollowMouse.apendages[FollowMouse.currentApendage].transform.position.x,
+            FollowMouse.apendages[FollowMouse.currentApendage].transform.position.y, transform.position.z);
         transform.position = Vector3.Lerp(transform.position, targetPos, Time.deltaTime * 10);
     }
 }
